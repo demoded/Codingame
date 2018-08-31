66ba78500a8778478440d50b908fa787e4544964
AShevchenko <AShevchenko@CLIENT159>
2018-08-31 16:13:25 +1200

    task text added

--- puzzle002chuck-norris/puzzle002chuck-norris/Program.cs
+++ puzzle002chuck-norris/puzzle002chuck-norris/Program.cs
@@ -4,6 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+https://www.codingame.com/ide/puzzle/chuck-norris
+Here is the encoding principle:
+    The input message consists of ASCII characters (7-bit)
+    The encoded output message consists of blocks of 0
+    A block is separated from another block by a space
+    Two consecutive blocks are used to produce a series of same value bits (only 1 or 0 values):
+    - First block: it is always 0 or 00. If it is 0, then the series contains 1, if not, it contains 0
+    - Second block: the number of 0 in this block is the number of bits in the series
+
+Example 1:
+    Let’s take a simple example with a message which consists of only one character: Capital C. C in binary is represented as 1000011, so with Chuck Norris’ technique this gives:
+    0 0 (the first series consists of only a single 1)
+    00 0000 (the second series consists of four 0)
+    0 00 (the third consists of two 1)
+    So C is coded as: 0 0 00 0000 0 00
+
+Example 2:
+    Encode the message CC (i.e. the 14 bits 10000111000011) :
+    0 0 (one single 1)
+    00 0000 (four 0)
+    0 000 (three 1)
+    00 0000 (four 0)
+    0 00 (two 1)
+    So CC is coded as: 0 0 00 0000 0 000 00 0000 0 00
+*/
+
 namespace puzzle002chuck_norris
 {
     class Program
