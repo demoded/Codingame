dbe869bb214deeca8c432d96b57e577032353399
Andy Shevchenko <demoded@hugaida.com>
2018-09-10 14:03:12 +1200

    commit user changed

---
+++ Puzzle007the-river/Puzzle007the-river/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puzzle007the_river
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
