99f39d977588d5572c75744ad4ccbf002406a831
Andy <gravisus@gmail.com>
2019-02-06 01:18:23 -0800

    task comment and test case added

--- Puzzle008bank-robbers/Puzzle008bank-robbers/Program.cs
+++ Puzzle008bank-robbers/Puzzle008bank-robbers/Program.cs
@@ -4,6 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+A gang of R foolish robbers decides to heist a bank. In the bank there are V vaults (indexed from 0 to V - 1). 
+The robbers have managed to extract some information from the bank's director:
+- The combination of a vault is composed of C characters (digits/vowels).
+- The first N characters consist of digits from 0 to 9.
+- The remaining characters consist of vowels (A, E, I, O, U).
+- C and N may be the same or different for different vaults.
+
+All the robbers work at the same time. A robber can work on one vault at a time, and a vault can be worked on by only one robber. 
+Robbers deal with the different vaults in increasing order.
+A robber tries the combinations at the speed of 1 combination per second. He tries all the possible combinations, 
+i.e. he continues to try the untried combinations even after he has found the correct combination. 
+Once he finishes one vault, he moves on to the next available vault, 
+that is the vault with the smallest index among all the vaults that have not been worked on yet. 
+The heist is finished when the robbers have worked on all the vaults.
+
+Assume it takes no time to move from one vault to another.
+You have to output the total time the heist takes. 
+
+Test case:
+    Input
+    1
+    1
+    3 1
+
+    Output:
+    250
+*/
+
 namespace Puzzle008bank_robbers
 {
     class Program
