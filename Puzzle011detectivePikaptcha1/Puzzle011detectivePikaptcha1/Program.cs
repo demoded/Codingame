827f631cf81d3cece702610234b1b0cf9edf226c
demoded <demoded@live.com>
2019-12-25 17:37:36 +1300

    easy peasy

---
+++ Puzzle011detectivePikaptcha1/Puzzle011detectivePikaptcha1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+You're given a grid filled with 0 and #, where 0 represents a passage, and # represents a wall: an impassable cell.
+We're considering the 4-adjacency, meaning a cell has a maximum of 4 adjacent cells (a diagonal cell is not adjacent).
+You must analyze the given grid and return it with a small transformation: 
+for each empty cell, instead of a 0, you must return the number of its adjacent passable cells. 
+For each impassable cell, you change nothing: you still return #. 
+*/
+
+namespace Puzzle011detectivePikaptcha1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string firstline = Console.ReadLine();
+            //Console.Error.WriteLine(firstline);
+            string[] inputs = firstline.Split(' ');
+            int width = int.Parse(inputs[0]);
+            int height = int.Parse(inputs[1]);
+            char[][] map = new char[height][];
+
+            //read map
+            for (int i = 0; i < height; i++)
+            {
+                string line = Console.ReadLine();
+                map[i] = line.ToCharArray();
+                //Console.Error.WriteLine(line);
+            }
+
+            //count adjacent and print out every line
+            for (int h = 0; h < height; h++)
+            {
+                string tempStr = string.Empty;
+                for (int w = 0; w < width; w++)
+                {
+                    if (map[h][w] == '#')
+                        tempStr += '#';
+                    else
+                        tempStr += CountAdjucentCells(h, w, map);
+                }
+                Console.WriteLine(tempStr);
+            }
+        }
+
+        private static char CountAdjucentCells(int h, int w, char[][] map)
+        {
+            int count = 0;
+
+            var maxh = map.Count()-1;
+            var maxw = map[0].Count()-1;
+
+            if (h > 0 && h < maxh)
+            {
+                count += map[h - 1][w] == '0' ? 1 : 0;
+                count += map[h + 1][w] == '0' ? 1 : 0;
+            }
+            else if (h == 0)
+                count += map[h + 1][w] == '0' ? 1 : 0;
+            else
+                count += map[h - 1][w] == '0' ? 1 : 0;
+
+            if (w > 0 && w < maxw)
+            {
+                count += map[h][w - 1] == '0' ? 1 : 0;
+                count += map[h][w + 1] == '0' ? 1 : 0;
+            }
+            else if (w == 0)
+                count += map[h][w + 1] == '0' ? 1 : 0;
+            else
+                count += map[h][w - 1] == '0' ? 1 : 0;
+
+            return count == 0 ? '#' : Convert.ToChar(count+48);
+        }
+    }
+}
---
+++ Puzzle011detectivePikaptcha1/UnitTest/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test01_A_Small_Chamber()
+        {
+            /*
+            5 3
+            0000#
+            #0#00
+            00#0#
+
+            1321#
+            #2#21
+            12#1#
+            */
+            string testcase = string.Format("5 3{0}0000#{0}#0#00{0}00#0#", Environment.NewLine);
+            string expected = string.Format("1322#{0}#2#31{0}12#1#", Environment.NewLine);
+
+            Assert.AreEqual<string>(expected, RunTest(testcase));
+        }
+        string RunTest(string _in)
+        {
+            string result;
+
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                using (StringReader sr = new StringReader(_in))
+                {
+                    Console.SetIn(sr);
+                    Puzzle011detectivePikaptcha1.Program.Main(new string[] { });
+
+                    result = sw.ToString().Trim();
+                }
+            }
+
+            return result;
+        }
+    }
+}
